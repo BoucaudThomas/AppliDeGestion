e1d180fa1e7f04659ca9d2e9eb8bdcb423d45e94
BOUCAUD Thomas <78614114+BoucaudThomas@users.noreply.github.com>
2022-05-17 19:15:04 -0400

    Delete replay/function directory

--- replay/function/resize/index.php
+++
@@ -1,6 +0,0 @@
-<form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <br><br>
-  <input type="submit" value="UploadImage" name="submit">
-</form>
